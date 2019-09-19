10cea2e90efba4f1c18b71e63d115af7890a6707
yicg <yichunguangyx@163.com>
2019-09-19 14:46:37 +0800

    封装

--- src/main/java/com/example/util/HttpClientUtils.java
+++ src/main/java/com/example/util/HttpClientUtils.java
@@ -1,14 +1,20 @@
 package com.example.util;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,7 +28,26 @@ public class HttpClientUtils {
 
 
     /**
-     * get请求
+     * get请求,不含请求头
+     * @param url
+     * @return
+     */
+    public static String doGet(String url){
+
+        HttpGet get=new HttpGet(url);
+        DefaultHttpClient client=new DefaultHttpClient();
+        try {
+            HttpResponse response=client.execute(get);
+            String result=EntityUtils.toString(response.getEntity(),"utf-8");
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * get请求,携带请求头
      * @param url
      * @return
      */
@@ -46,7 +71,7 @@ public class HttpClientUtils {
     }
 
     /**
-     * 普通post请求
+     * 普通post请求,json请求参数
      * @param url
      * @param param
      * @return
@@ -74,4 +99,45 @@ public class HttpClientUtils {
         }
        return null;
     }
+
+
+    /**
+     * 普通post请求,key-value
+     * @param url
+     * @param param
+     * @return
+     */
+    public static String doPost(String url,Map<String,Object> param,Map<String,String> headers){
+
+        HttpPost post=new HttpPost(url);
+        DefaultHttpClient client=new DefaultHttpClient();
+
+       try {
+
+        //封装请求头
+        for (Map.Entry<String, String> header : headers.entrySet()){
+            post.setHeader(header.getKey(),header.getValue());
+        }
+
+        //装配post请求参数
+        List<NameValuePair> list = new ArrayList<NameValuePair>();
+        for (String key : param.keySet()) {
+            list.add(new BasicNameValuePair(key, String.valueOf(param.get(key))));
+        }
+
+        //将参数进行编码为合适的格式,如将键值对编码为param1=value1&param2=value2
+        UrlEncodedFormEntity entity=new UrlEncodedFormEntity(list,"utf-8");
+        post.setEntity(entity);
+
+        HttpResponse response=client.execute(post);
+        String result=EntityUtils.toString(response.getEntity(),"utf-8");
+
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
