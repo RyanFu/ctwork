be79f6f6a8e8bf79759b3cd210dbe9e7b9e2cff1
yicg <yichunguangyx@163.com>
2019-09-20 11:19:30 +0800

    指定打印目录

--- src/main/java/com/example/test/InitClass.java
+++ src/main/java/com/example/test/InitClass.java
@@ -2,7 +2,10 @@ package com.example.test;
 
 
 import com.example.util.DatabaseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
+import java.util.List;
 
 /**
  * @Author: yicg
@@ -13,15 +16,22 @@ import org.testng.annotations.Test;
 
 public class InitClass {
 
+    public static Logger logger = LoggerFactory.getLogger(InitClass.class);
+
     String name1="databaseConfig.xml";
     String name2="databaseConfig-001.xml";
 
     @Test
     public void init() {
+        List list=DatabaseUtil.getSqlSession(name1).selectList("findAll");
         System.out.println(DatabaseUtil.getSqlSession(name1).selectList("findAll"));
         System.out.println(DatabaseUtil.getSqlSession(name2).selectList("findAll"));
         System.out.println(DatabaseUtil.getSqlSession(name2).selectList("findList"));
 
+        logger.info("11111111");
+        logger.warn("2222222");
+        logger.debug("111111333311");
+        logger.error("444444");
 
 
     }
