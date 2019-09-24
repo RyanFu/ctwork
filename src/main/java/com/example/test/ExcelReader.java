82279a23b0c5e794589e1b372b87f4fc4f35f2b0
yicg <yichunguangyx@163.com>
2019-09-24 11:46:19 +0800

    数据驱动修改

--- src/main/java/com/example/test/ExcelReader.java
+++ src/main/java/com/example/test/ExcelReader.java
@@ -1,12 +1,10 @@
 package com.example.test;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-
-import java.io.File;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -15,81 +13,73 @@ import java.util.List;
 public class ExcelReader {
 
 
-    /**
-     * 读取excel文件，excel中不含合并单元格
-     * @param path
-     * @param sheetName
-     * excel表格要注意每行的最后一格不能为空
-     * 如果要为空，则需要手动随便填写字符，然后再按del键删除才可以
-     * 目前没找到这个解决办法，后续会继续查找解决办法
-     * @return
-     */
-    public static String[][] getExpectationData(String path, String sheetName) {
-        try {
-            File file = new File(path);
-            FileInputStream fis = new FileInputStream(file);
-            POIFSFileSystem POIStream = new POIFSFileSystem(fis);
-            HSSFWorkbook workBook = new HSSFWorkbook(POIStream);
-            //得到工作表
-            HSSFSheet sheet1 = workBook.getSheet(sheetName);
-            //得到总行数
-            int rowNum = sheet1.getLastRowNum();
-            List<String[]> results = new ArrayList<String[]>();
-            for (int i=1;i<=rowNum;i++){
-                //当前行
-                HSSFRow row = sheet1.getRow(i);
-                int colNum = row.getLastCellNum();
-                String[] data = new String[colNum];
-                //当前行所有列
-                for (int j = 0; j < colNum; j++) {
-                    try {
-                        data[j] = getCellValue(row.getCell(j));
-                    }catch (NullPointerException e){ //如果单元格为空的时候，则用这个来处理
-                        data[j] = "";
-                    }
-                }
-                //把data[]数组的数据存在list<[]>中
-                results.add(data);
-            }
-            fis.close();
+    public static XSSFSheet ExcelWSheet;
+    public static XSSFWorkbook ExcelWBook;
+    public static XSSFCell Cell;
+    public static XSSFRow Row;
 
-            String[][] returnArray = new String[results.size()][rowNum];
-            for (int i = 0; i < returnArray.length; i++) {
-                returnArray[i] = (String[]) results.get(i);
-            }
-            return returnArray;
-        }catch (Exception e){
+    public static String[][] getExpectationData(String file, String sheetName) {
+
+            try {
+                FileInputStream ExcelFile = new FileInputStream(file);//获取Excel文件
+                ExcelWBook = new XSSFWorkbook(ExcelFile);
+                // 得到工作表名称
+                ExcelWSheet = ExcelWBook.getSheet(sheetName);
+                // 得到总行数
+                int rowNum = ExcelWSheet.getLastRowNum();
+                List<String[]> results = new ArrayList<String[]>();
+                for (int i = 1; i <= rowNum; i++) {
+                    // 当前行
+                    XSSFRow row = ExcelWSheet.getRow(i);
+                    int colNum = row.getLastCellNum();
+                    String[] data = new String[colNum];
+                    // 当前行所有列
+                    for (int j = 0; j < colNum; j++) {
+                        try {
+                            data[j] = getCellValue(row.getCell(j));
+                        } catch (NullPointerException e) { // 如果单元格为空的时候，则用这个来处理
+                            data[j] = "";
+                        }
+                    }
+                    // 把data[]数组的数据存在list<[]>中
+                    results.add(data);
+                }
+                String[][] returnArray = new String[results.size()][rowNum];
+                for (int i = 0; i < returnArray.length; i++) {
+                    returnArray[i] = (String[]) results.get(i);
+                }
+                return returnArray;
+
+            }catch (Exception e){
             return null;
         }
+
     }
-
-
-
 
 
     /**
      * 对Excel的各个单元格的格式进行判断并转换
      */
-    public static String getCellValue(HSSFCell cell) {
+    public static String getCellValue(XSSFCell xssfCell) {
         String cellValue = "";
         DecimalFormat df = new DecimalFormat("#");
-        switch (cell.getCellType()) {
+        switch (xssfCell.getCellType()) {
             case HSSFCell.CELL_TYPE_STRING:
-                cellValue =cell.getRichStringCellValue().getString().trim();
+                cellValue = xssfCell.getRichStringCellValue().getString().trim();
                 break;
             case HSSFCell.CELL_TYPE_NUMERIC:
-                cellValue =df.format(cell.getNumericCellValue()).toString();
+                cellValue = df.format(xssfCell.getNumericCellValue()).toString();
                 break;
             case HSSFCell.CELL_TYPE_BOOLEAN:
-                cellValue = String.valueOf(cell.getBooleanCellValue()).trim();
+                cellValue = String.valueOf(xssfCell.getBooleanCellValue()).trim();
                 break;
             case HSSFCell.CELL_TYPE_FORMULA:
-                cellValue =cell.getCellFormula();
+                cellValue = xssfCell.getCellFormula();
                 break;
             default:
                 cellValue = "";
         }
         return cellValue;
-    }
+     }
 
-}
+ }
